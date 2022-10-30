b90860d5d19e9d44e1128bf06c965bc079c75f2c
Abdourahamane <amdroudjae@gmail.com>
2022-10-30 23:27:06 +0100

    Mise à jour des liens du menu de navigation dans la vue App_Shopping/resources/views/pages/apropos.blade.php

--- App_Shopping/resources/views/pages/apropos.blade.php
+++ App_Shopping/resources/views/pages/apropos.blade.php
@@ -15,9 +15,9 @@
             <a class="navbar-brand" href="#">Laravel 7.X</a>
             </div>
             <ul class="nav navbar-nav">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Apropos</a></li>
-                <li><a href="#">Services</a></li>
+                <li><a href="{{ URL::to('/') }}">Home</a></li>
+                <li><a href="{{ URL::to('/apropos') }}">Apropos</a></li>
+                <li><a href="{{ URL::to('/services') }}">Services</a></li>
                 <li><a href="https://www.udemy.com/course/laravel-creer-un-site-e-commerce-de-a-a-z-avec-carte-credit/?referralCode=D3F3089F215BB2933CFD">Apprendre plus</a></li>
             </ul>
              <!--<ul class="nav navbar-nav navbar-right">
