f675335cb170537f348ff7c7cf69381875481e18
Abdourahamane <amdroudjae@gmail.com>
2022-10-30 22:51:25 +0100

    Création de la route pour la page apropos

--- App_Shopping/routes/web.php
+++ App_Shopping/routes/web.php
@@ -20,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages/home');
 });
+
+Route::get('/apropos', function () {
+    return view('pages/apropos');
+});
