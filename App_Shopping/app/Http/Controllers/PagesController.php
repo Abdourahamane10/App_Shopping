04933bdda7abc68760fc6863ab3527d1a7afc9ca
Abdourahamane <amdroudjae@gmail.com>
2022-11-05 20:32:37 +0100

    Modification du nom du champ Product

--- App_Shopping/app/Http/Controllers/PagesController.php
+++ App_Shopping/app/Http/Controllers/PagesController.php
@@ -2,21 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class PagesController extends Controller
 {
+    /*
+    Fonction qui redirige vers la page d'accueil
+    */
     public function home()
     {
         return view('pages/home');
     }
 
+    /*
+    Fonction qui redirige vers la page apropos
+    */
     public function apropos()
     {
         return view('pages/apropos');
     }
 
+    /*
+    Fonction qui affiche la liste des produits
+    */
     public function services()
     {
         $produits = DB::table('Products')
@@ -25,18 +35,32 @@ class PagesController extends Controller
         return view('pages/services')->with('produits', $produits);
     }
 
+    /*
+    Fonction qui affiche les details d'un produit
+    */
     public function show($id)
     {
         $produit = DB::table('Products')->where('id', $id)->first();
         return view('pages.show')->with('produit', $produit);
     }
 
-    public function create()
+    /*
+    Fonction qui affiche le formulaire de création d'un produit
+    */
+    public function creer()
     {
-        return view('pages.create');
+        return view('pages.ajouterProduit');
     }
 
-    public function sauverproduit()
+    /*
+    Fonction permettant d'enregistrer les produits dans la base de données
+    */
+    public function create(Request $request)
     {
+        $produit = new Product();
+        $produit->Product_name = $request->Product_name;
+        $produit->Product_price = $request->Product_price;
+        $produit->description = $request->Product_description;
+        $produit->save();
     }
 }
