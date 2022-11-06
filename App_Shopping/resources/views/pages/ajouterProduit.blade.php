8185608ba06eb124a424393c98390967bd6240f4
Abdourahamane <amdroudjae@gmail.com>
2022-11-06 20:54:16 +0100

    Utilisation de laravel collective à la place des balises html simples

--- App_Shopping/resources/views/pages/ajouterProduit.blade.php
+++ App_Shopping/resources/views/pages/ajouterProduit.blade.php
@@ -11,20 +11,32 @@ Sauver produit
     {{ Session::put('message', null) }}
 </p>
 @endif
-<form action="{{ url('/sauverproduit') }}" method="POST" class="form_horizontal">
+{!! Form::open(['action' => 'App\Http\Controllers\PagesController@create']) !!}
+{{-- <form action="{{ url('/sauverproduit') }}" method="POST" class="form_horizontal"> --}}
     {{ csrf_field() }}
     <div class="form-group">
-        <label>Product</label>
-        <input type="text" name="Product_name" placeholder="Product Name" class="form-control" required>
+        {{ Form::label('', 'Product') }}
+        {{ Form::text('Product_name', '', ['placeholder' => 'Product Name', 'class' => 'form-control'], 'required') }}
+        {{--<label>Product</label>
+        <input type="text" name="Product_name" placeholder="Product Name" class="form-control" required>--}}
     </div>
     <div class="form-group">
-        <label>Product_price</label>
-        <input type="text" name="Product_price" placeholder="Product Price" class="form-control" required>
+        {{ Form::label('', 'Product_price') }}
+        {{ Form::number('Product_price', '', ['placeholder' => 'Product Price', 'class' => 'form-control'], 'required')
+        }}
+        {{--<label>Product_price</label>
+        <input type="number" name="Product_price" placeholder="Product Price" class="form-control" required>--}}
     </div>
     <div class="form-group">
-        <label>Product description</label>
-        <textarea name="Product_description" cols="30" rows="10" class="form-control" required></textarea>
+        {{ Form::label('', 'Product description') }}
+        {{ Form::textarea('Product_description', '', ['placeholder' => 'Product description', 'class' =>
+        'form-control'], 'required') }}
+        {{--<label>Product description</label>
+        <textarea name="Product_description" cols="30" rows="10" class="form-control" required></textarea>--}}
     </div>
-    <input type="submit" value="Ajouter Produit" class="btn btn-primary">
-</form>
+    {{ Form::submit('Ajouter Produit', ['class' => 'btn btn-primary']) }}
+    {{--<input type="submit" value="Ajouter Produit" class="btn btn-primary">--}}
+    {!! Form::close() !!}
+    {{--
+</form>--}}
 @endsection
