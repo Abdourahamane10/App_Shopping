411d7178847a417fdfab7bf2f6712e1ae876f6fe
Abdourahamane <amdroudjae@gmail.com>
2022-11-11 22:03:58 +0100

    Création de la route appellant la fonction modifierProduit du contrôleur PagesController après avoir cliqué sur le bouton Modifier Produit

--- App_Shopping/resources/views/pages/services.blade.php
+++ App_Shopping/resources/views/pages/services.blade.php
@@ -6,6 +6,12 @@ Services
 
 @section('contenu')
 <h1>Bienvenu dans la page Services</h1>
+@if (Session::has('message'))
+<p class="alert alert-success">
+    {{ Session::get('message') }}
+    {{ Session::put('message', null) }}
+</p>
+@endif
 @foreach ($produits as $produit)
 <div class="well">
     <h1><a href="/show/{{ $produit->id }}"> {{ $produit->Product_name }}</a></h1>
